065ba6f49181eff2e54d38d8b0c7484f1d4773b3
carlosramosa <carlosramos.cramos@gmail.com>
2019-05-25 10:05:52 +0200

    feat(): productor and consumer now are ready

---
+++ consumidor/ksteam.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*
+    { value: '{"quality":720,"ping":149}' }
+*/
+
+const {KafkaStreams} = require("kafka-streams");
+const config = require ('./config.json');
+
+const kafkaStreams = new KafkaStreams(config);
+
+kafkaStreams.on("error", (error) => {
+    console.log("Error occured:", error.message);
+});
+
+//creating a ktable requires a function that can be
+//used to turn the kafka messages into key-value objects
+//as tables can only be built on key-value pairs
+
+const keyValueMapperEtl = ({ value, key })  => {
+
+    const message = { 'key': ( typeof key !== 'string' ? 'youtube' : key) + Date.now(), 'value': JSON.parse(value) };
+
+    console.log ('Message consumed  ---->  ' + JSON.stringify(message));
+
+    return message;
+}
+
+const makeSearch = () => {
+    
+  
+}
+
+
+const run = async () => {
+    makeSearch();
+
+    setTimeout (() => {
+        run ();
+    }, 1000)
+};
+
+run ()
+    .catch (console.error);
--- productor/index.js
+++ productor/index.js
@@ -1,31 +1,41 @@
-const { Kafka } = require('kafkajs')
- 
+const { Kafka } = require('kafkajs');
+
+const TOPIC = 'mitopic';
+
 const kafka = new Kafka({
     clientId: 'my-app',
     brokers: ['localhost:9092']
 })
 
+const generateMessage = () => (
+    { key: 'youtube', value: JSON.stringify({quality: qualities[Math.floor(Math.random() * 6) ], ping: Math.floor(Math.random() * 200) + 1 })}
+);
+
+const qualities = [360, 480, 720, 1080, 1440, 2160];
+
 const producer = kafka.producer()
 const consumer = kafka.consumer({ groupId: 'test-group' })
-let message = '';
+//let messages = [];
 
 const run = async () => {
   // Producing
     await producer.connect()
-    message = { value: JSON.stringify({quality: 720, ping: Math.floor(Math.random() * 200) + 1 })};
+    //message = { key: 'youtube', value: JSON.stringify({quality: qualities[Math.floor(Math.random() * 6) ], ping: Math.floor(Math.random() * 200) + 1 })};
+    const messages = [
+        generateMessage ()
+        , generateMessage ()
+        , generateMessage ()
+    ];
     await producer.send({
         topic: 'mitopic',
-        messages: [
-            { value: JSON.stringify({quality: 720, ping: Math.floor(Math.random() * 200) + 1 })}
-        ]
+        messages
     });
     setTimeout((() => {
-        console.log (message);
+        console.log (messages);
         run();
     }), 1000);
 }
 
 run ()
-    // .then(process.exit(0))
     .catch (console.error);
 
